accdd11a60a56f499248bf3166533b1bfc0997a0
yiluansong <yiluansong@gmail.com>
2023-09-06 15:15:36 -0400

    util_fill_whit

--- R/util_whit.R
+++ R/util_whit.R
@@ -3,8 +3,9 @@
 #' This function performs Whittaker smoothing based on ptw:whit1. ptw::whit1 does not take NA in the time series. This function takes a time series with NA or placeholder value (e.g., -9999). Weights at placeholder should be set to 0. NA are skipped, while values at placeholders are interpolated.
 #'
 #' @param x signal to be smoothed: a vector
+#' @param maxgap Maximum number of successive NAs to still perform imputation on. Default setting is to replace all NAs without restrictions.
 #' @param lambda smoothing parameter: larger values lead to more smoothing
-#' @param w weights: a vector of same length as y. Default weights are equal to one
+#' @param minseg Minimum length of segments to perform Whittaker smoothing on. Default setting is to 2.
 #'
 #' @return smoothed signal: a vector
 #'
@@ -12,18 +13,27 @@
 #' # Example:
 #' data.frame(
 #'   doy = 1:365, # simulate some data
-#'   evi = sin(doy)
+#'   evi = sin((1:365) / 365 * pi)
 #' ) %>%
-#'   filter(doy <= 20 | doy >= 40) %>% # make some data missing
+#'   filter(doy <= 20 | doy >= 40) %>% # make some short segments of data missing
+#'   filter(doy <= 40 | doy >= 80) %>% # make some log segments of data missing
 #'   complete(doy = seq(1, 365, by = 1)) %>%
-#'   mutate(evi_fill = zoo::na.fill(evi, fill = -9999, maxgap = 14)) %>% # fill short gaps with -9999 placeholder
-#'   mutate(w = (evi_fill != -9999)) %>% # weight = 0 at long gaps, weight = 1 at short gaps
-#'   mutate(evi_sm = whitgap(x = evi, lambda = 50, w = w)) %>% # weighted whittaker smoothing
-#'   select(-evi_fill, -w)
+#'   mutate(evi_sm = util_fill_whit(x = evi, maxgap = 14, lambda = 50, minseg = 2)) %>% # weighted whittaker smoothing allowing gaps %>%
+#'   ggplot() +
+#'   geom_line(aes(x = doy, y = evi_sm), col = "blue")
 #' @export
 
 
-whitgap <- function(x, lambda, w) {
+util_fill_whit <- function(x, maxgap = Inf, lambda, minseg = 2) {
+  x_fill <- imputeTS::na_replace(x, fill = -9999, maxgap = maxgap) # fill short gaps with -9999 placeholder
+  w <- (x_fill != -9999) # weight = 0 at long gaps, weight = 1 at short gaps
+  x_sm <- util_whit(x = x_fill, lambda = lambda, w = w, minseg = minseg)
+
+  return(x_sm)
+}
+
+
+util_whit <- function(x, lambda, w, minseg = 2) {
   max_id <- 0
   done <- F
   while (!done) {
@@ -39,8 +49,13 @@ whitgap <- function(x, lambda, w) {
       } else {
         max_id <- min(v_na) - 1 + (min_id - 1) # index of last number in this NA segment
       }
-      x[min_id:max_id] <- ptw::whit1(x[min_id:max_id], lambda, w[min_id:max_id]) # whitman smoothing for this non-NA segment
+      if (max_id - min_id + 1 < minseg) { # this non-NA segment is too short
+        x[min_id:max_id] <- -9999
+      } else {
+        x[min_id:max_id] <- ptw::whit1(x[min_id:max_id], lambda, w[min_id:max_id]) # whitman smoothing for this non-NA segment
+      }
     }
   }
+  x[x == -9999] <- NA
   return(x)
 }
